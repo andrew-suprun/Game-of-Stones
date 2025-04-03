55c6eada088978a704c9edf31c45020a470b709f
Andrew Suprun <andrew.suprun@gmail.com>
2025-04-03 16:05:51 -0400

    make heap a generic type

--- Zig/src/heap.zig
+++ Zig/src/heap.zig
@@ -1,51 +1,73 @@
-const std = @import("std");
-const ArrayList = std.ArrayList;
+pub fn Heap(comptime T: type, comptime size: usize, comptime less: fn (T, T) bool) type {
+    return struct {
+        const Self = @This();
 
-pub fn heapAdd(comptime T: type, comptime max_items: isize, comptime less: fn (T, T) bool, item: T, items: *ArrayList(T)) void {
-    if (items.items.len == max_items) {
-        if (!less(items.items[0], item)) return;
+        _items: [size]T,
+        len: usize,
 
-        items.items[0] = item;
-        siftDown(T, less, items);
-        return;
-    }
-    items.append(item) catch {};
-    siftUp(T, less, items);
-}
-
-fn siftUp(comptime T: type, less: fn (T, T) bool, items: *ArrayList(T)) void {
-    var child_idx = items.items.len - 1;
-    const child = items.items[child_idx];
-    while (child_idx > 0 and less(child, items.items[(child_idx - 1) / 2])) {
-        const parent_idx = (child_idx - 1) / 2;
-        items.items[child_idx] = items.items[parent_idx];
-        child_idx = parent_idx;
-    }
-    items.items[child_idx] = child;
-}
-
-fn siftDown(comptime T: type, less: fn (T, T) bool, items: *ArrayList(T)) void {
-    var idx: usize = 0;
-    const elem = items.items[idx];
-    while (true) {
-        var first = idx;
-        const left_child_idx = idx * 2 + 1;
-        if (left_child_idx < items.items.len and less(items.items[left_child_idx], elem)) {
-            first = left_child_idx;
+        pub fn init() Self {
+            return Self{
+                ._items = undefined,
+                .len = 0,
+            };
         }
-        const right_child_idx = idx * 2 + 2;
-        if (right_child_idx < items.items.len and
-            less(items.items[right_child_idx], elem) and
-            less(items.items[right_child_idx], items.items[left_child_idx]))
-        {
-            first = right_child_idx;
-        }
-        if (idx == first) break;
 
-        items.items[idx] = items.items[first];
-        idx = first;
-    }
-    items.items[idx] = elem;
+        pub fn add(self: *Self, item: T) void {
+            if (self.len == size) {
+                if (!less(self._items[0], item)) return;
+
+                self._items[0] = item;
+                self.siftDown();
+                return;
+            }
+            self._items[self.len] = item;
+            self.len += 1;
+            self.siftUp();
+        }
+
+        pub fn items(self: *Self) []T {
+            return self._items[0..self.len];
+        }
+
+        pub fn clear(self: *Self) void {
+            self.len = 0;
+        }
+
+        fn siftUp(self: *Self) void {
+            var child_idx = self.len - 1;
+            const child = self._items[child_idx];
+            while (child_idx > 0 and less(child, self._items[(child_idx - 1) / 2])) {
+                const parent_idx = (child_idx - 1) / 2;
+                self._items[child_idx] = self._items[parent_idx];
+                child_idx = parent_idx;
+            }
+            self._items[child_idx] = child;
+        }
+
+        fn siftDown(self: *Self) void {
+            var idx: usize = 0;
+            const elem = self._items[idx];
+            while (true) {
+                var first = idx;
+                const left_child_idx = idx * 2 + 1;
+                if (left_child_idx < self.len and less(self._items[left_child_idx], elem)) {
+                    first = left_child_idx;
+                }
+                const right_child_idx = idx * 2 + 2;
+                if (right_child_idx < self.len and
+                    less(self._items[right_child_idx], elem) and
+                    less(self._items[right_child_idx], self._items[left_child_idx]))
+                {
+                    first = right_child_idx;
+                }
+                if (idx == first) break;
+
+                self._items[idx] = self._items[first];
+                idx = first;
+            }
+            self._items[idx] = elem;
+        }
+    };
 }
 
 fn testLess(i: isize, j: isize) bool {
@@ -53,46 +75,35 @@ fn testLess(i: isize, j: isize) bool {
 }
 
 test "heapAdd" {
-    var items = ArrayList(isize).init(std.testing.allocator);
+    var heap = Heap(isize, 20, testLess).init();
 
     for (0..100) |i| {
-        heapAdd(isize, 20, testLess, @intCast(i * 17 % 100), &items);
+        const v: isize = @intCast(i * 17 % 100);
+        heap.add(v);
     }
 
+    const items = heap.items();
     for (1..20) |i| {
-        const parent = items.items[(i - 1) / 2];
-        const child = items.items[i];
+        const parent = items[(i - 1) / 2];
+        const child = items[i];
+        std.debug.print("t: i: {} p: {}, c: {}\n", .{ i, parent, child });
         try std.testing.expect(parent < child);
     }
-    items.deinit();
 }
 
 // Benchmark
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-
-    const allocator = arena.allocator();
-
-    var items = ArrayList(isize).init(allocator);
-
+    var heap = Heap(isize, 20, testLess).init();
     var timer = try std.time.Timer.start();
+
     for (0..5) |_| {
         for (0..1_000_000) |_| {
-            items.clearRetainingCapacity();
+            heap.clear();
             for (0..100) |i| {
-                heapAdd(isize, 20, testLess, @intCast(i * 17 % 100), &items);
-            }
-
-            for (1..20) |i| {
-                const parent = items.items[(i - 1) / 2];
-                const child = items.items[i];
-                try std.testing.expect(parent < child);
+                heap.add(@intCast(i * 17 % 100));
             }
         }
         const dur = timer.lap();
         std.debug.print("{d} sec\n", .{@as(f64, @floatFromInt(dur)) / 1_000_000_000});
     }
-
-    items.deinit();
 }
