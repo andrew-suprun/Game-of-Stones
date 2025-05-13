acb1b98ef7fd8a6190106afb02a75b4948be0240
Andrew Suprun <andrew.suprun@gmail.com>
2025-05-13 15:24:57 -0400

    generalize heapAdd()

--- Swift/Sources/Heap/Heap.swift
+++ Swift/Sources/Heap/Heap.swift
@@ -1,24 +1,24 @@
 import Foundation
 
  @inlinable 
- public func heap_add<T: Comparable>(_ item: T, to items: inout [T], maxItems: Int) {
+ public func heapAdd<T>(_ item: T, to items: inout [T], maxItems: Int, less: (T, T) -> Bool) {
     if items.count == maxItems {
-        if item < items[0] {
+        if !less(items[0], item) {
             return
         }
         items[0] = item
-        siftDown(&items)
+        siftDown(&items, less)
         return
     }
     items.append(item)
-    siftUp(&items)
+    siftUp(&items, less)
 }
 
 @inlinable 
-func siftUp<T: Comparable>(_ items: inout [T]) {
+func siftUp<T>(_ items: inout [T], _ less: (T, T) -> Bool) {
     var childIdx = items.count - 1
     let child = items[childIdx]
-    while childIdx > 0 && child < items[(childIdx - 1) / 2] {
+    while childIdx > 0 && less(child, items[(childIdx - 1) / 2]) {
         let parentIdx = (childIdx - 1) / 2
         items[childIdx] = items[parentIdx]
         childIdx = parentIdx
@@ -27,19 +27,19 @@ func siftUp<T: Comparable>(_ items: inout [T]) {
 }
 
 @inlinable 
-func siftDown<T: Comparable>(_ items: inout [T]) {
+func siftDown<T>(_ items: inout [T], _ less: (T, T) -> Bool) {
     var idx = 0
     let elem = items[idx]
     while true {
         var first = idx
         let leftChildIdx = idx * 2 + 1
-        if leftChildIdx < items.count && items[leftChildIdx] < elem {
+        if leftChildIdx < items.count && less(items[leftChildIdx], elem) {
             first = leftChildIdx
         }
         let rightChildIdx = idx * 2 + 2
         if rightChildIdx < items.count
-            && items[rightChildIdx] < elem
-            && items[rightChildIdx] < items[leftChildIdx]
+            && less(items[rightChildIdx], elem)
+            && less(items[rightChildIdx], items[leftChildIdx])
         {
             first = rightChildIdx
         }
--- Swift/Tests/HeapTest.swift
+++ Swift/Tests/HeapTest.swift
@@ -11,7 +11,7 @@ import Testing
     values.shuffle()
 
     for i in 0..<100 {
-        heap_add(values[i], to: &items, maxItems: 20)
+        heapAdd(values[i], to: &items, maxItems: 20) { $0 < $1 }
     }
 
     for i in 1..<20 {
